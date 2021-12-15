949daf1bcb522039917273d5e98d255f2e3cb2e8
abdelfattah115 <abdelfattahkhaled54@gmail.com>
2021-12-15 09:30:52 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/flutter/first_app/first_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter.first_app.first_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/modules/new_tasks_screen/new_tasks.dart
@@ -0,0 +1,49 @@
+
+import 'package:first_app/shared/components/components.dart';
+import 'package:first_app/shared/cubit/cubit.dart';
+import 'package:first_app/shared/cubit/states.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class NewTasks_Screen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<AppCubit , AppStates>(
+      listener: (context , state){},
+      builder: (context , state){
+        var tasks = AppCubit.get(context).newTasks;
+
+        return tasks.length > 0 ? ListView.separated(
+          itemBuilder: (context ,index) => buildTaskItem(tasks[index],context),
+          separatorBuilder: (context ,index) => Container(
+            width: double.infinity,
+            height: 1.0,
+            color: Colors.grey[300],
+          ),
+          itemCount: tasks.length,
+        ) : Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.menu,
+                size: 100,
+                color: Colors.grey,
+              ),
+              Text(
+                'No tasks yet, please add some Tasks',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
